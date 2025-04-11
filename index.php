fb4758ffbe29a00a72d02a5b9ee34d57e2a059a0
William Grossman <willg.@Williams-MacBook-Air-2.local>
2025-04-10 20:02:23 -0500

    Included index.php file

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Ch14</h1>";
+
+echo "<ul>";
+echo "<li><a href='ch14-proj1.php'>View CH14 Project.php</a></li>";
+echo "<li><a href='detail.php?id=1'>Example CH14 Detail.php</a></li>";
+?>
